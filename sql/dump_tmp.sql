d0ca2c4feb78f0dfdf74c8fb17b29aae93c44932
Jean-Baptiste Aubry <jean-baptiste.aubry@ac-besancon.fr>
2022-10-12 14:30:35 +0200

    test

--- sql/dump_tmp.sql
+++ sql/dump_tmp.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: cs_cafe
 -- ------------------------------------------------------
--- Server version	10.4.22-MariaDB
+-- Server version	10.4.24-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -333,4 +333,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-12  5:52:22
+-- Dump completed on 2022-10-12 14:29:41
